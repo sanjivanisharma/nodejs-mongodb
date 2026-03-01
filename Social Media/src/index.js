5850fb77347fce197e72221e1b597c8fbf4e25b8
Sanjivani Sharma <sanjivanisharma01@gmail.com>
2026-03-01 21:36:19 +0530

    Added app configuration

--- Social Media/src/index.js
+++ Social Media/src/index.js
@@ -1,5 +1,16 @@
 // require('dotenv').config() - Below is the better approach
 import {} from "dotenv/config"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
+
+const PORT = process.env.PORT || 8000
 
 connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on ${PORT}`)
+    })
+})
+.catch((error) => {
+    console.error("Failed to connect to DB!! ", error)
+})
